c2708373122dbc6bb24879e443082b5abce27c64
jmartinez0837 <jmartinez@riverdale.edu>
2015-04-02 12:19:36 -0400

    Update overnightMunki.sh

--- overnightMunki.sh
+++ overnightMunki.sh
@@ -11,15 +11,12 @@ TurnOnTime=02
 
 if [ "$SystemHour" = "$TurnOnTime" ] ; then
 
-		#set closing lid not to sleep laptop
-		/usr/local/bin/NoSleepCtrl -a -b -s 1,1
-	
-		#gives time for noSleep extension to load
-		sleep 5
-
 		#check system battery 
 		battPercentage=$(pmset -g batt | grep "InternalBattery-0" | cut -c 21-23 | sed s/\%//)
 		if [ "$battPercentage" -gt 50 ] ; then
+		
+			#wait until we can talk to munki server before continuing
+			until /sbin/ping -c 1 -t 90 yourServer.example.com; do /sbin/sleep 3; done
 		
 			#use --auto in case laptop does go to sleep, when opened there will be no visual to the user and they can still log in
 			/usr/local/munki/managedsoftwareupdate --auto
@@ -32,8 +29,6 @@ if [ "$SystemHour" = "$TurnOnTime" ] ; then
 else
 
 	echo "Its not time to run updates."
-	#set closing lid back to sleep laptop
-	/usr/local/bin/NoSleepCtrl -a -b -s 0,0
 fi
 
 exit 0
